434b4a48767160a8eabb6620307158f45f0736c3
Roman <kowaliszyn@gazeta.pl>
2024-05-13 21:59:41 +0200

    ExponentialDecayLearningRate

--- src/MachineLearning/NeuralNetwork/LearningRates/ExponentialDecayLearningRate.cs
+++ src/MachineLearning/NeuralNetwork/LearningRates/ExponentialDecayLearningRate.cs
@@ -9,7 +9,13 @@ public class ExponentialDecayLearningRate(float initialLearningRate, float final
     private readonly float _initialLearningRate = initialLearningRate;
     private readonly float _finalLearningRate = finalLearningRate;
 
-    public override void Update(int epoch, int epochs) => CurrentLearningRate = _initialLearningRate * (float)Math.Pow(_finalLearningRate / _initialLearningRate, (float)epoch / epochs);
+    public override void Update(int epoch, int epochs) { 
+        if(epochs == 1)
+            CurrentLearningRate = _initialLearningRate;
+        else
+            CurrentLearningRate = _initialLearningRate * (float)Math.Pow(_finalLearningRate / _initialLearningRate, (float)(epoch - 1) / (epochs - 1));
+        Console.WriteLine($"CurrentLearningRate: {CurrentLearningRate}, epoch: {epoch}, epochs: {epochs}");
+    }
 
     public override string ToString() => $"ExponentialDecayLearningRate (initialLearningRate={_initialLearningRate}, finalLearningRate={_finalLearningRate})";
 }
--- src/MachineLearning/NeuralNetwork/LearningRates/LinearDecayLearningRate.cs
+++ src/MachineLearning/NeuralNetwork/LearningRates/LinearDecayLearningRate.cs
@@ -9,7 +9,13 @@ public class LinearDecayLearningRate(float initialLearningRate, float finalLearn
     private readonly float _initialLearningRate = initialLearningRate;
     private readonly float _finalLearningRate = finalLearningRate;
 
-    public override void Update(int epoch, int epochs) => CurrentLearningRate = _initialLearningRate - (_initialLearningRate - _finalLearningRate) * epoch / epochs;
+    public override void Update(int epoch, int epochs) 
+    { 
+        if(epochs == 1)
+            CurrentLearningRate = _initialLearningRate;
+        else
+            CurrentLearningRate = _initialLearningRate - (_initialLearningRate - _finalLearningRate) * (epoch - 1) / (epochs - 1); 
+    }
 
     public override string ToString() => $"LinearDecayLearningRate (initialLearningRate={_initialLearningRate}, finalLearningRate={_finalLearningRate})";
 }
