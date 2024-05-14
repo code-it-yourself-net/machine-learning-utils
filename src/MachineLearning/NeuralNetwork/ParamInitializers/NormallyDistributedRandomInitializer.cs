39b31d58aaacf045a735e8200f8d086a7cd92ca6
Roman <kowaliszyn@gazeta.pl>
2024-05-14 08:32:14 +0200

    GlorotInitializer

---
+++ src/MachineLearning/NeuralNetwork/ParamInitializers/NormallyDistributedRandomInitializer.cs
@@ -0,0 +1,14 @@
+﻿// Machine Learning Utils
+// File name: NormallyDistributedRandomInitializer.cs
+// Code It Yourself with .NET, 2024
+
+namespace MachineLearning.NeuralNetwork.ParamInitializers;
+
+public class NormallyDistributedRandomInitializer(float mean = 0, float stdDev = 1, int? seed = null) : RandomInitializer(seed)
+{
+    internal override Matrix InitBiases(int neurons) => Matrix.RandomNormal(1, neurons, Random, mean, stdDev);
+
+    internal override Matrix InitWeights(int inputColumns, int neurons) => Matrix.RandomNormal(inputColumns, neurons, Random, mean, stdDev);
+
+    public override string ToString() => $"NormallyDistributedRandomInitializer (seed={Seed}, mean={mean}, stdDev={stdDev})";
+}
