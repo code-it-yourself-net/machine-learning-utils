1d9bb21b0c1e0a832cd18530ced18073e3b2fc6e
Roman <kowaliszyn@gazeta.pl>
2024-05-18 12:12:00 +0200

    Unit tests

---
+++ tests/MachineLearning.Tests/TypedMatrixTests.cs
@@ -0,0 +1,266 @@
+// Machine Learning Utils
+// File name: MatrixTests.cs
+// Code It Yourself with .NET, 2024
+
+namespace MachineLearning.Tests;
+
+[TestClass]
+public class TypedMatrixTests
+{
+    [TestMethod]
+    public void ZerosTest()
+    {
+        TypedMatrix matrix = TypedMatrix.Zeros(2, 3);
+        Assert.AreEqual(2, matrix.GetDimension(Dimension.Rows));
+        Assert.AreEqual(3, matrix.GetDimension(Dimension.Columns));
+        Assert.AreEqual(0f, matrix.Array.GetValue(0, 0));
+        Assert.AreEqual(0f, matrix.Array.GetValue(1, 2));
+    }
+
+    [TestMethod]
+    public void OnesTest()
+    {
+        TypedMatrix matrix = TypedMatrix.Ones(3, 2);
+        Assert.AreEqual(3, matrix.GetDimension(Dimension.Rows));
+        Assert.AreEqual(2, matrix.GetDimension(Dimension.Columns));
+        Assert.AreEqual(1f, matrix.Array.GetValue(0, 0));
+        Assert.AreEqual(1f, matrix.Array.GetValue(2, 1));
+    }
+
+    [TestMethod]
+    public void RandomTest()
+    {
+        Random random = new();
+        TypedMatrix matrix = TypedMatrix.Random(2, 2, random);
+        Assert.AreEqual(2, matrix.GetDimension(Dimension.Rows));
+        Assert.AreEqual(2, matrix.GetDimension(Dimension.Columns));
+        Assert.IsTrue((float)matrix.Array.GetValue(0, 0)! >= -0.5f && (float)matrix.Array.GetValue(0, 0)! <= 0.5f);
+        Assert.IsTrue((float)matrix.Array.GetValue(1, 1)! >= -0.5f && (float)matrix.Array.GetValue(1, 1)! <= 0.5f);
+    }
+
+    [TestMethod]
+    public void AddTest()
+    {
+        TypedMatrix matrix = new(new float[,] { { 1, 2 }, { 3, 4 } });
+        TypedMatrix result = matrix.Add(2);
+        Assert.AreEqual(3f, result.Array.GetValue(0, 0));
+        Assert.AreEqual(6f, result.Array.GetValue(1, 1));
+    }
+
+    [TestMethod]
+    public void AddRowTest()
+    {
+        TypedMatrix matrix = new(new float[,] { { 1, 2 }, { 3, 4 } });
+        TypedMatrix row = new(new float[,] { { 5, 6 } });
+        TypedMatrix result = matrix.AddRow(row);
+        Assert.AreEqual(6f, result.Array.GetValue(0, 0));
+        Assert.AreEqual(8f, result.Array.GetValue(0, 1));
+        Assert.AreEqual(8f, result.Array.GetValue(1, 0));
+        Assert.AreEqual(10f, result.Array.GetValue(1, 1));
+    }
+
+    [TestMethod]
+    public void MultiplyTest()
+    {
+        TypedMatrix matrix = new(new float[,] { { 1, 2 }, { 3, 4 } });
+        TypedMatrix result = matrix.Multiply(2);
+        Assert.AreEqual(2f, result.Array.GetValue(0, 0));
+        Assert.AreEqual(8f, result.Array.GetValue(1, 1));
+    }
+
+    [TestMethod]
+    public void PowerTest()
+    {
+        TypedMatrix matrix = new(new float[,] { { 1, 2 }, { 3, 4 } });
+        TypedMatrix result = matrix.Power(2);
+        Assert.AreEqual(1f, result.Array.GetValue(0, 0));
+        Assert.AreEqual(16f, result.Array.GetValue(1, 1));
+    }
+
+    [TestMethod]
+    public void MultiplyDotTest()
+    {
+        TypedMatrix matrix1 = new(new float[,] { { 1, 2 }, { 3, 4 } });
+        TypedMatrix matrix2 = new(new float[,] { { 5, 6 }, { 7, 8 } });
+        TypedMatrix result = matrix1.MultiplyDot(matrix2);
+        Assert.AreEqual(19f, result.Array.GetValue(0, 0));
+        Assert.AreEqual(22f, result.Array.GetValue(0, 1));
+        Assert.AreEqual(43f, result.Array.GetValue(1, 0));
+        Assert.AreEqual(50f, result.Array.GetValue(1, 1));
+    }
+
+    [TestMethod]
+    public void MultiplyElementwiseTest1()
+    {
+        TypedMatrix matrix1 = new(new float[,] { { 1, 2 }, { 3, 4 } });
+        TypedMatrix matrix2 = new(new float[,] { { 5, 6 }, { 7, 8 } });
+        TypedMatrix result = matrix1.MultiplyElementwise(matrix2);
+        Assert.AreEqual(5f, result.Array.GetValue(0, 0));
+        Assert.AreEqual(12f, result.Array.GetValue(0, 1));
+        Assert.AreEqual(21f, result.Array.GetValue(1, 0));
+        Assert.AreEqual(32f, result.Array.GetValue(1, 1));
+    }
+
+    [TestMethod]
+    public void MultiplyRowElementwiseTest1()
+    {
+        TypedMatrix matrix1 = new(new float[,] { { 1, 2 }, { 3, 4 } });
+        TypedMatrix matrix2 = new(new float[,] { { 5, 6 } });
+        TypedMatrix result = matrix1.MultiplyElementwise(matrix2);
+        Assert.AreEqual(5f, result.Array.GetValue(0, 0));
+        Assert.AreEqual(12f, result.Array.GetValue(0, 1));
+        Assert.AreEqual(15f, result.Array.GetValue(1, 0));
+        Assert.AreEqual(24f, result.Array.GetValue(1, 1));
+    }
+
+    [TestMethod]
+    public void MultiplyRowElementwiseTest2()
+    {
+        TypedMatrix matrix1 = new(new float[,] { { 5, 6 } });
+        TypedMatrix matrix2 = new(new float[,] { { 1, 2 }, { 3, 4 } });
+        TypedMatrix result = matrix1.MultiplyElementwise(matrix2);
+        Assert.AreEqual(5f, result.Array.GetValue(0, 0));
+        Assert.AreEqual(12f, result.Array.GetValue(0, 1));
+        Assert.AreEqual(15f, result.Array.GetValue(1, 0));
+        Assert.AreEqual(24f, result.Array.GetValue(1, 1));
+    }
+
+    [TestMethod]
+    public void MultiplyColumnElementwiseTest1()
+    {
+        TypedMatrix matrix1 = new(new float[,] { { 1, 2 }, { 3, 4 } });
+        TypedMatrix matrix2 = new(new float[,] { { 5 }, { 6 } });
+        TypedMatrix result = matrix1.MultiplyElementwise(matrix2);
+        Assert.AreEqual(5f, result.Array.GetValue(0, 0));
+        Assert.AreEqual(10f, result.Array.GetValue(0, 1));
+        Assert.AreEqual(18f, result.Array.GetValue(1, 0));
+        Assert.AreEqual(24f, result.Array.GetValue(1, 1));
+    }
+
+    [TestMethod]
+    public void MultiplyColumnElementwiseTest2()
+    {
+        TypedMatrix matrix1 = new(new float[,] { { 5 }, { 6 } });
+        TypedMatrix matrix2 = new(new float[,] { { 1, 2 }, { 3, 4 } });
+        TypedMatrix result = matrix1.MultiplyElementwise(matrix2);
+        Assert.AreEqual(5f, result.Array.GetValue(0, 0));
+        Assert.AreEqual(10f, result.Array.GetValue(0, 1));
+        Assert.AreEqual(18f, result.Array.GetValue(1, 0));
+        Assert.AreEqual(24f, result.Array.GetValue(1, 1));
+    }
+
+    [TestMethod]
+    public void SubtractTest()
+    {
+        TypedMatrix matrix1 = new(new float[,] { { 1, 2 }, { 3, 4 } });
+        TypedMatrix matrix2 = new(new float[,] { { 5, 6 }, { 7, 8 } });
+        TypedMatrix result = matrix1.Subtract(matrix2);
+        Assert.AreEqual(-4f, result.Array.GetValue(0, 0));
+        Assert.AreEqual(-4f, result.Array.GetValue(0, 1));
+        Assert.AreEqual(-4f, result.Array.GetValue(1, 0));
+        Assert.AreEqual(-4f, result.Array.GetValue(1, 1));
+    }
+
+    [TestMethod]
+    public void MeanTest()
+    {
+        TypedMatrix matrix = new(new float[,] { { 1, 2 }, { 3, 4 } });
+        float result = matrix.Mean();
+        Assert.AreEqual(2.5f, result);
+    }
+
+    [TestMethod]
+    public void SumTest()
+    {
+        TypedMatrix matrix = new(new float[,] { { 1, 2 }, { 3, 4 } });
+        float result = matrix.Sum();
+        Assert.AreEqual(10, result);
+    }
+
+    [TestMethod]
+    public void SumByTest()
+    {
+        TypedMatrix matrix = new(new float[,] { { 1, 2 }, { 3, 4 } });
+        TypedMatrix result = matrix.SumBy(Dimension.Rows);
+        Assert.AreEqual(1, result.GetDimension(Dimension.Rows));
+        Assert.AreEqual(4f, result.Array.GetValue(0, 0));
+        Assert.AreEqual(6f, result.Array.GetValue(0, 1));
+    }
+
+    [TestMethod]
+    public void GetRowTest()
+    {
+        TypedMatrix matrix = new(new float[,] { { 1, 2 }, { 3, 4 } });
+        TypedMatrix row = matrix.GetRow(1);
+        Assert.AreEqual(1, row.GetDimension(Dimension.Rows));
+        Assert.AreEqual(2, row.GetDimension(Dimension.Columns));
+        Assert.AreEqual(3f, row.Array.GetValue(0, 0));
+        Assert.AreEqual(4f, row.Array.GetValue(0, 1));
+    }
+
+    [TestMethod]
+    public void GetColumnTest()
+    {
+        TypedMatrix matrix = new(new float[,] { { 1, 2 }, { 3, 4 } });
+        TypedMatrix row = matrix.GetColumn(1);
+        Assert.AreEqual(2, row.GetDimension(Dimension.Rows));
+        Assert.AreEqual(1, row.GetDimension(Dimension.Columns));
+        Assert.AreEqual(2f, row.Array.GetValue(0, 0));
+        Assert.AreEqual(4f, row.Array.GetValue(1, 0));
+    }
+
+    [TestMethod]
+    public void SetRowTest()
+    {
+        TypedMatrix matrix = new(new float[,] { { 1, 2 }, { 3, 4 } });
+        TypedMatrix row = new(new float[,] { { 5, 6 } });
+        matrix.SetRow(1, row);
+        Assert.AreEqual(1f, matrix.Array.GetValue(0, 0));
+        Assert.AreEqual(2f, matrix.Array.GetValue(0, 1));
+        Assert.AreEqual(5f, matrix.Array.GetValue(1, 0));
+        Assert.AreEqual(6f, matrix.Array.GetValue(1, 1));
+    }
+
+    [TestMethod]
+    public void GetRowsTest()
+    {
+        TypedMatrix matrix = new(new float[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } });
+        TypedMatrix submatrix = matrix.GetRows(1..3);
+        Assert.AreEqual(2, submatrix.GetDimension(Dimension.Rows));
+        Assert.AreEqual(2, submatrix.GetDimension(Dimension.Columns));
+        Assert.AreEqual(3f, submatrix.Array.GetValue(0, 0));
+        Assert.AreEqual(4f, submatrix.Array.GetValue(0, 1));
+        Assert.AreEqual(5f, submatrix.Array.GetValue(1, 0));
+        Assert.AreEqual(6f, submatrix.Array.GetValue(1, 1));
+    }
+
+    [TestMethod]
+    public void GetColumnsTest()
+    {
+        TypedMatrix matrix = new(new float[,] { { 1, 2, 3 }, { 3, 4, 5 }, { 5, 6, 7 } });
+        TypedMatrix submatrix = matrix.GetColumns(1..3);
+        Assert.AreEqual(3, submatrix.GetDimension(Dimension.Rows));
+        Assert.AreEqual(2, submatrix.GetDimension(Dimension.Columns));
+        Assert.AreEqual(2f, submatrix.Array.GetValue(0, 0));
+        Assert.AreEqual(3f, submatrix.Array.GetValue(0, 1));
+        Assert.AreEqual(4f, submatrix.Array.GetValue(1, 0));
+        Assert.AreEqual(5f, submatrix.Array.GetValue(1, 1));
+        Assert.AreEqual(6f, submatrix.Array.GetValue(2, 0));
+        Assert.AreEqual(7f, submatrix.Array.GetValue(2, 1));
+    }
+
+    [TestMethod]
+    public void TransposeTest()
+    {
+        TypedMatrix matrix = new(new float[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } });
+        TypedMatrix transposed = matrix.Transpose();
+        Assert.AreEqual(2, transposed.GetDimension(Dimension.Rows));
+        Assert.AreEqual(3, transposed.GetDimension(Dimension.Columns));
+        Assert.AreEqual(1f, transposed.Array.GetValue(0, 0));
+        Assert.AreEqual(2f, transposed.Array.GetValue(1, 0));
+        Assert.AreEqual(3f, transposed.Array.GetValue(0, 1));
+        Assert.AreEqual(4f, transposed.Array.GetValue(1, 1));
+        Assert.AreEqual(5f, transposed.Array.GetValue(0, 2));
+        Assert.AreEqual(6f, transposed.Array.GetValue(1, 2));
+    }
+}
